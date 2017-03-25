7c7f2ddc5d58f7f22291ced5e96e1d59fc595e9a
Aleksey <aleksey.bestuzhev@gmail.com>
2017-03-25 22:50:59 +0300

    changes

--- local/templates/bazby/js/functions.js
+++ local/templates/bazby/js/functions.js
@@ -428,3 +428,18 @@ $(window).on('resize', function () {
     navMobileSubmenuFix();
 
 });
+
+
+$(document).ready(function () {
+  $(".nav-main__item").on("click", function(e){
+    e.preventDefault();
+    $(this).parents(".nav-main-list__item").siblings().find(".nav-main__submenu").removeClass('show-mobile-menu')
+    $(this).siblings(".nav-main__submenu").toggleClass('show-mobile-menu');
+  });
+
+  $(".nav-main__submenu-menu-block").on("click", function(e){
+    e.preventDefault();
+    $(this).siblings().children(".submenu-3").removeClass('show-mobile-menu');
+    $(this).children(".submenu-3").addClass('show-mobile-menu');
+  });
+});
